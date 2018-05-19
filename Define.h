f36963c14c227470987b6b75a86712315a60c020
Yuri Sanas <sanasy.yuri@gmail.com>
2018-05-19 05:03:01 -0300

    Update Define.h

--- Define.h
+++ Define.h
@@ -3,12 +3,12 @@
 #define	ELESIS_THIRD	2
 #define	ELESIS_FOURTH	3
 
-#define	LIRE_FIRST		10
-#define	LIRE_SECOND		11
-#define	LIRE_THIRD		12
-#define	LIRE_FOURTH		13
+#define	LIRE_FIRST	10
+#define	LIRE_SECOND	11
+#define	LIRE_THIRD	12
+#define	LIRE_FOURTH	13
 
-#define	ARME_FIRST		20
-#define	ARME_SECOND		21
-#define	ARME_THIRD		22
-#define	ARME_FOURTH		23
+#define	ARME_FIRST	20
+#define	ARME_SECOND	21
+#define	ARME_THIRD	22
+#define	ARME_FOURTH	23
